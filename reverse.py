385b5f33e13b785ac1c37c12908c1ff8902be2ab
kat <katharinebrooks@hotmail.co.uk>
2022-04-01 17:17:01 +0100

    reverse function working, but needs to be moved inside triple quotes

--- reverse.py
+++ reverse.py
@@ -33,10 +33,27 @@ a = np.random.randint(1000, size=100000)
 
 '''
 reverse_function = '''
-def sort_data():
-    a.sort()
+def reverse_data():
+    i=0
+    n=int(len(arr)/2)
+    print(n)
+  while i < n:
+    print(i)
 '''
-
-it = np.nditer(np.random.randint(1000, size=10), flags=['f_index'])
-for x in it:
-  print(x, it.index, end=' ')
+def reverse_data():
+    a = np.random.randint(1000, size=11)
+    i=0
+    length = len(a)
+    n=int(length/2)
+    print(a)
+    while i < n:
+        first = a[i]
+        last = a[length-1-i]
+        a[i] = last
+        a[length-1-i] = first 
+        i += 1
+    print(a)
+reverse_data()
+# it = np.nditer(np.random.randint(1000, size=10), flags=['f_index'])
+# for x in it:
+#   print(x, it.index, end=' ')
